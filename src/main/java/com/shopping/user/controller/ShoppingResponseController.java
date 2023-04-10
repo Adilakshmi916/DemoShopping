67e03d10841e4d9e59fd28c6b45800f9ecfbca8c
siddiqnassar <nassarsiddiq@gmail.com>
2023-04-10 17:39:03 +0530

    added division

--- src/main/java/com/shopping/user/controller/ShoppingResponseController.java
+++ src/main/java/com/shopping/user/controller/ShoppingResponseController.java
@@ -5,7 +5,6 @@ import java.awt.PageAttributes.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
